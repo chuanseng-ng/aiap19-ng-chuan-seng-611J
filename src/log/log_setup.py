d313fc9b148bdd4c9f3e372e54e33c610737569b
Ng Chuan Seng <waelectriz@gmail.com>
2024-12-05 22:33:10 +0800

    [Task_2] Add feature to delete log file if exists during new run

--- src/log/log_setup.py
+++ src/log/log_setup.py
@@ -1,5 +1,11 @@
+import os
 import logging
 
+log_file = "ml_model.log"
+
+# Check if log file exists and delete if exists
+if os.path.exists(log_file):
+    os.remove(log_file)
 
 # Create logger
 logger = logging.getLogger(__name__)
@@ -19,7 +25,7 @@ formatter = logging.Formatter("%(levelname)s - %(message)s")
 # logger.addHandler(ch)
 
 # Add file logging
-fh = logging.FileHandler("ml_model.log")
+fh = logging.FileHandler(log_file)
 fh.setLevel(logging.INFO)
 fh.setFormatter(formatter)
 logger.addHandler(fh)
