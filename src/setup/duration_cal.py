25b5cf4befa997c2993456f78f039058280dea11
Ng Chuan Seng <waelectriz@gmail.com>
2024-12-05 13:05:20 +0800

    [Task_2] Remove print

--- src/setup/duration_cal.py
+++ src/setup/duration_cal.py
@@ -18,7 +18,6 @@ def duration_cal(duration: float):
 
 # Print out runtime and return current time, takes in time value to offset
 def duration_print(input_time: float, step_cnt: int):
-    print(type(input_time))
     current_time = time.time()
     current_duration, current_tag = duration_cal(current_time - input_time)
     print(f"Part {step_cnt} has run for {current_duration:.3f} {current_tag}!")
